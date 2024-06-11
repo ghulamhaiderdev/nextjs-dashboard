b7fdb6a0bd8a33815656a31438da8a894d099482
ghulamhaiderdev <ghulamhaider082@gmail.com>
2024-06-11 19:22:21 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+export const inter  = Inter({subsets: ['latin'] });
+
+export const lutanist = Lusitana({
+    weight : ['400', '700'],
+    subsets: ['latin'],
+
+});
+
